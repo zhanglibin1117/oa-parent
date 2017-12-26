1f4ee6119a2875a68c5a7914b67f0aa82b9bfc55
zhss <zhss@163.com>
2017-12-26 18:37:44 +0800

    add one line on branch feature/002

--- oa-auth/src/main/java/com/zhss/oa/Test1.java
+++ oa-auth/src/main/java/com/zhss/oa/Test1.java
@@ -8,5 +8,6 @@ public class Test1 {
 		System.out.println("再次测试一下代码");
 		System.out.println("这是在feature/001分支上写的代码");
 		System.out.println("再测试一把");
+		System.out.println("这是在feature/002上写的代码");
 	}
 }
