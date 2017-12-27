8fdb754dcc270c15fad5c7ff708b78d406d13146
zhss <zhss@163.com>
2017-12-27 18:46:33 +0800

    I am 李四，这是我提交的

--- oa-organ/src/main/java/com/zhss/oa/TestOrgan.java
+++ oa-organ/src/main/java/com/zhss/oa/TestOrgan.java
@@ -2,6 +2,6 @@ package com.zhss.oa;
 
 public class TestOrgan {
    public static void main(String[] args) {
-	System.out.println("我是李四，这是测试organ，我已经修改了");
+	System.out.println("我是李四，这是测试organ，我已经修改了,张三不要修改了");
 }
 }
