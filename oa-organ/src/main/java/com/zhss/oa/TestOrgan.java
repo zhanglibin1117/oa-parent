014a9588587df47020b7c8cbbb095ff632c53ff3
zhss <zhss@163.com>
2017-12-27 19:31:51 +0800

    我是张三，这是我提交的

--- oa-organ/src/main/java/com/zhss/oa/TestOrgan.java
+++ oa-organ/src/main/java/com/zhss/oa/TestOrgan.java
@@ -4,5 +4,6 @@ public class TestOrgan {
    public static void main(String[] args) {
 	System.out.println("我是李四，这是测试organ，我已经修改了,张三不要修改了");
 	System.out.println("我是张三，这是测试organ，我已经修改了,李四不要修改");
+	System.out.println("我是张三，我要再测试一次");
 }
 }
