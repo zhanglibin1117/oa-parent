ce4a9789b17b9eab62414ab3bab4c1980e850639
lisi <lisi@163.com>
2017-12-29 10:10:40 +0800

    我是张三，这是我测的rebase

--- oa-organ/src/main/java/com/zhss/oa/TestOrgan2.java
+++ oa-organ/src/main/java/com/zhss/oa/TestOrgan2.java
@@ -5,6 +5,6 @@ public class TestOrgan2 {
 	System.out.println("这是测试2");
 	System.out.println("我是张三,我写了一个测试类,我是在feature/001上写的");
 	System.out.println("我是张三,我写了一个测试类,我是在feature/002上编写的");
-
+    System.out.println("我是张三,我再测试rebase");
 }
 }
