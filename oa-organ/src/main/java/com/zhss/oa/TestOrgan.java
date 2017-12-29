3de76ff571d8ccc3b4c750e078328e2d817927d4
lisi <lisi@163.com>
2017-12-29 10:03:41 +0800

    我是李四，这是我测试的rebase

--- oa-organ/src/main/java/com/zhss/oa/TestOrgan.java
+++ oa-organ/src/main/java/com/zhss/oa/TestOrgan.java
@@ -6,5 +6,6 @@ public class TestOrgan {
 	System.out.println("我是李四，我也要再测试一下,哈哈哈哈");
 	System.out.println("我是张三，这是测试organ，我已经修改了,李四不要修改");
 	System.out.println("我是张三，我要再测试一次");
+	System.out.println("我是李四，我要测试一下rebase");
 }
 }  
