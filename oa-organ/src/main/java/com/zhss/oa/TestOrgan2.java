f49728106c5d5cc303c949ab8dd7d0dd4852e2e2
lisi <lisi@163.com>
2017-12-29 10:52:54 +0800

    我是张三，我测试--set-upstream-to

--- oa-organ/src/main/java/com/zhss/oa/TestOrgan2.java
+++ oa-organ/src/main/java/com/zhss/oa/TestOrgan2.java
@@ -6,5 +6,6 @@ public class TestOrgan2 {
 	System.out.println("我是张三,我写了一个测试类,我是在feature/001上写的");
 	System.out.println("我是张三,我写了一个测试类,我是在feature/002上编写的");
     System.out.println("我是张三,我再测试rebase");
+    System.out.println("我是张三,我测试--set-upstream-to");
 }
 }
